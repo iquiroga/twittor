b4f9ea147ade89df124edbed2305687cb1481bed
iquiroga <ignacioequiroga@gmail.com>
2021-06-11 20:59:38 -0300

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts("js/sw-utils.js");
 
-const STATIC_CACHE = "static-v3";
+const STATIC_CACHE = "static-v4";
 const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
@@ -43,6 +43,10 @@ self.addEventListener("activate", (e) => {
   const respuesta = caches.keys().then((keys) => {
     keys.forEach((key) => {
       if (key !== STATIC_CACHE && key.includes("static")) {
+        return caches.delete(key);
+      }
+
+      if (key !== DYNAMIC_CACHE && key.includes("dynamic")) {
         return caches.delete(key);
       }
     });
